3fbfd1247cb22288c52b5dee86be590c7e765619
jannatulnayeem964 <115387717+jannatulnayeem964@users.noreply.github.com>
2024-01-01 13:17:10 -0500

    Finished Stage 2; Currently working on Stage 3

--- index.js
+++ index.js
@@ -29,20 +29,25 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+    for (let i = 0; i < games.length; i++) {
         // create a new div element, which will become the game card
-
+        const gameCard = document.createElement("div");
 
         // add the class game-card to the list
-
+        element.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
+        gameCard.innerHTML = `
+            <img src="${games[i].img}" class= "game-img">
+            <h3>${games[i].name}</h3>
+            <p>Pledged: $${games[i].pledged}</p>
+            <p>Backers: ${games[i].backers}</p>
+        `;
 
-
+    }
         // append the game to the games-container
 
 }
